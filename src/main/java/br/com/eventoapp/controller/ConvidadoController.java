b92f818dc13fc3dd6e0a7b0f4dfc7a0535b1aaf9
rafael.fernandes <rafael.fernandes@MJV-000563.mshome.net>
2020-08-26 20:13:05 -0300

    Ajustes Atributos Redirect

--- src/main/java/br/com/eventoapp/controller/ConvidadoController.java
+++ src/main/java/br/com/eventoapp/controller/ConvidadoController.java
@@ -3,7 +3,6 @@ package br.com.eventoapp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 import br.com.eventoapp.model.Convidado;
@@ -21,7 +20,7 @@ public class ConvidadoController {
 	private ConvidadoRepository convidadoRepository;
 
 	@PostMapping(value = "/incluirConvidado")
-	public RedirectView incluirConvidado(Convidado convidado, RedirectAttributes attributes) {
+	public RedirectView incluirConvidado(Convidado convidado) {
 		Evento evento = eventoRepository.getOne(convidado.getEvento().getCodigo());
 		
 		convidado.setEvento(evento);
